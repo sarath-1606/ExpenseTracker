36ec72e7b27e55932472e7d41c8346d53ac1df94
304516 <nairsharath068@gmail.com>
2026-01-17 10:03:07 +0530

    added user details

--- src/main/java/com/example/expensetracker/models/User.java
+++ src/main/java/com/example/expensetracker/models/User.java
@@ -1,4 +1,13 @@
 package com.example.expensetracker.models;
 
+import org.springframework.data.annotation.Id;
+
 public class User {
+
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
 }
